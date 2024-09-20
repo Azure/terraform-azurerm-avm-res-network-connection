ad361dc990e465b0597622f4cbea84ac3722170b
Mauricio Rojas Martinez <mauricio.rojas@microsoft.com>
2024-09-19 22:36:32 -0500

    Add: Completed optional variables

--- main.tf
+++ main.tf
@@ -1,9 +1,33 @@
 # TODO: Replace this dummy resource azurerm_resource_group.TODO with your module resource
-resource "azurerm_resource_group" "TODO" {
-  location = var.location
-  name     = var.name # calling code must supply the name
-  tags     = var.tags
+resource "azurerm_virtual_network_gateway_connection" "onpremise" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  type                       = var.type
+  shared_key = var.shared_key
+  virtual_network_gateway_id = var.virtual_network_gateway_id
+
+  #Non Required
+  local_network_gateway_id   = var.local_network_gateway_id
+
+ 
+  dynamic "ipsec_policy" {
+    for_each = var.ipsec_policy 
+    content {
+      sa_lifetime = ipsec_policy.value.sa_lifetime
+      sa_datasize = ipsec_policy.value.sa_datasize
+      ipsec_encryption = ipsec_policy.value.ipsec_encryption
+      ipsec_integrity = ipsec_policy.value.ipsec_integrity
+      ike_encryption = ipsec_policy.value.ike_encryption
+      ike_integrity = ipsec_policy.value.ike_integrity
+      dh_group = ipsec_policy.value.dh_group
+      pfs_group = ipsec_policy.value.pfs_group
+    }
+    ## If this was an object, remove dynamic, for_each and content
+  }
 }
+
+
 
 # required AVM resources interfaces
 resource "azurerm_management_lock" "this" {
